870f30db93ac1b1ec12bcb879b3640ee41112959
tanji2014 <tomoko.tanji@finc.com>
2020-01-18 10:18:37 +0900

    state修正

---
+++ my-app/src/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Profile extends React.Component {
+	render(){
+		return(
+			<div className='profile-item'>
+				<div className='profile-name'>
+					<p>名前：{this.props.name}</p>
+				</div>
+				<div className='profile-bio'>
+			 		<p>紹介文：{this.props.bio}</p>
+				</div>
+				<img className='profile-image' src={this.props.image}/>
+			</div>
+					
+		);
+	}
+}
+
+export default Profile;
